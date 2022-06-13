353406d6eea172f3723b8c0a12aa681e6c288971
Gilbert Twesigomwe <gilbert@bposeats.com>
2022-06-13 06:05:53 +0300

    added global error handling

--- src/main.js
+++ src/main.js
@@ -1,15 +1,33 @@
 import { createApp, provide, h  } from 'vue'
 import { DefaultApolloClient } from '@vue/apollo-composable'
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client/core'
+import { onError } from '@apollo/client/link/error'
 import App from './App.vue'
-
-const cache = new InMemoryCache()
 
 const httpLink = createHttpLink({uri:  'http://localhost:8000/graphql' })
 
+//Error Handling
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if(process.env.NODE_ENV !== 'production') {
+    if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    )
+
+    if (networkError) console.log(`[Network error]: ${networkError}`)
+  }
+})
+
+const cache = new InMemoryCache()
+
+
+
 const apolloClient = new ApolloClient({
   cache,
-  link: httpLink,
+  link: errorLink.concat(httpLink),
+  connectToDevTools: true,
 })
 
 const app = createApp({
